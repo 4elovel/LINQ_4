aa4ae11185a7f2274b13e4b347150471706703a8
4elovel <marcynakvova007@gmail.com>
2023-04-18 23:08:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = new List<int>();
+            var list = new List<int>() {1,45,8,4,14,78,347,98,1,45,8,4,6};
+            list.Sort();
+            list.Distinct();
+            var linq = from i in list
+                       select i;
+            var ex = linq;
+            foreach (var x in ex) {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
